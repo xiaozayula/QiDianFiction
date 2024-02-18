e2deb8048b4906244e0c6d3515066f1519bb8fc5
xiaozayula <981446636@qq.com>
2024-02-18 23:12:44 +0800

    1.注册接口的参数校验

--- src/main/java/com/xiaoyu/controller/UserController.java
+++ src/main/java/com/xiaoyu/controller/UserController.java
@@ -3,18 +3,21 @@ package com.xiaoyu.controller;
 import com.xiaoyu.pojo.Result;
 import com.xiaoyu.pojo.User;
 import com.xiaoyu.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("register")
-    public Result register(String userName,String passWord) {
+    public Result register(@Pattern(regexp="^\\S{5,16}$") String userName,@Pattern(regexp="^\\S{5,16}$")  String passWord) {
         if (userName != null && userName.length() <= 16 && userName.length() >= 5
                 && passWord != null && passWord.length() <= 16 && passWord.length() >= 5) {
             //查询用户
---
+++ src/main/java/com/xiaoyu/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.xiaoyu.exception;
+
+import com.mysql.cj.util.StringUtils;
+import com.xiaoyu.pojo.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * Created with IntelliJ IDEA.
+ * 参数校验失败的异常处理
+ * @Author: zhangyu
+ * @Date: 2024/02/18/15:37
+ * @Description:
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public Result handleException(Exception e){
+        e.printStackTrace();
+        return Result.error(StringUtils.hasLength(e.getMessage())? e.getMessage() : "操作失败");
+    }
+}
