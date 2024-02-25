c13a3adde6f54752467078d61b4a4d3091534804
xiaozayula <981446636@qq.com>
2024-02-25 15:54:44 +0800

    分组校验

--- src/main/java/com/xiaoyu/controller/CategoryController.java
+++ src/main/java/com/xiaoyu/controller/CategoryController.java
@@ -22,7 +22,7 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
     @PostMapping
-    public Result add(@RequestBody @Validated Category category){
+    public Result add(@RequestBody @Validated(Category.Add.class) Category category){
         categoryService.add(category);
         return Result.success();
     }
@@ -38,7 +38,7 @@ public class CategoryController {
         return Result.success(c);
     }
     @PutMapping
-    public  Result update(@RequestBody @Validated Category category){
+    public  Result update(@RequestBody @Validated (Category.Update.class)Category category){
         categoryService.update(category);
         return  Result.success();
     }
--- src/main/java/com/xiaoyu/pojo/Category.java
+++ src/main/java/com/xiaoyu/pojo/Category.java
@@ -9,15 +9,25 @@ import java.time.LocalDateTime;
 
 @Data
 public class Category {
-    @NotNull//不能不传
+    @NotNull(groups=Update.class)//不能不传
     private Integer id;// 主键ID
-    @NotEmpty//不能不传且不能为空
+    @NotEmpty(groups={Add.class,Update.class})//不能不传且不能为空
     private String categoryName;// 分类名称
-    @NotEmpty
+    @NotEmpty(groups={Add.class,Update.class})
     private String categoryAlias;// 分类别名
     private Integer createUser;// 创建人ID
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;// 创建时间
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;// 更新时间
+
+    //如果某个校验组没有指定分组，默认值是Default分组
+    //分组之间可以继承，A enxtends B，那么A有B中所有的校验组
+
+    public  interface Add{
+
+    }
+    public  interface Update{
+
+    }
 }
