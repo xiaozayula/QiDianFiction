62acff6ab45427e3c9b36594490da973074aa62f
xiaozayula <981446636@qq.com>
2024-02-25 20:52:23 +0800

    修改bug

--- src/main/java/com/xiaoyu/mapper/CategoryMapper.java
+++ src/main/java/com/xiaoyu/mapper/CategoryMapper.java
@@ -18,8 +18,8 @@ import java.util.List;
 @Mapper
 public interface CategoryMapper  {
     //新增
-    @Insert("insert  into category(category_name,category_alias,create_time,update_time)"+
-    "values (#{categoryName},#{categoryAlias},#{categoryUser},#cateTimes),#{updateTime}")
+    @Insert("insert  into category(category_name,category_alias,create_user,create_time,update_time)"+
+    "values (#{categoryName},#{categoryAlias},#{createUser},#{createTime},#{updateTime})")
     void add(Category category);
 
     //查询所有
